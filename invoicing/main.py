baffdbb473c91c09dc3c36598554994726c03b11
Alex Raichev <alex@raichev.net>
2016-04-17 19:20:04 +1200

    Working on tiered billing

--- invoicing/main.py
+++ invoicing/main.py
@@ -3,6 +3,8 @@ Main module.
 
 A *timesheet* is a Pandas data frame with at least the columns...
 """
+from collections import OrderedDict
+
 import pandas as pd 
 import numpy as np 
 from jinja2 import Environment, PackageLoader
@@ -12,31 +14,64 @@ from jinja2 import Environment, PackageLoader
 ENV = Environment(loader=PackageLoader('main', 'templates'))
 
 # I/O
+def parse_date(date_str):
+    if date_str is None:
+        return None
+    return pd.datetime.strptime(date_str, '%Y%m%d')
+
 def read_timesheet(path):
     """
     Read a timesheet located at the given path and return the corresponding
     data frame.
     """
-    parser = lambda date: pd.datetime.strptime(date, '%Y%m%d')
-    f = pd.read_csv(path, parse_dates=['date'], date_parser=parser)
+    f = pd.read_csv(path, parse_dates=['date'], date_parser=parse_date)
     f = f.sort_values('date')
+    # Convert from minutes to hours
+    f['time_spent'] /= 60
     return f
 
 def validate_timesheet(timesheet):
     pass
 
+def slice_by_dates(timesheet, date1=None, date2=None):
+    d1, d2 = map(parse_date, [date1, date2])
+    return timesheet.copy().set_index('date')[d1:d2].reset_index()
+    
 # Billing
-def bill_at_constant_rate(timesheet, project_rates_df):
+def bill_at_project_rates(timesheet, project_rates_df,
+  date1=None, date2=None):
     """
-    Append a ``cost`` column to the given timesheet data frame and return 
-    the resulting data frame.
+    Slice the given timesheet to the given dates,
+    and return a new data frame with the columns
 
-    Billing business logic goes here.
+    - ``'project'``
+    - ``'time_spent'``
+    - ``'rate'``: currency per hour
+    - ``'cost'``: time spent multiplied by rate
+
+    Get the rates per project from the data frame
+    ``project_rates_df``, which has the columns:
+
+    - ``'project'``
+    - ``'rate'``.
     """
-    f = timesheet.copy()
-    g = f.merge(project_rates_df)
-    g['cost'] = (g['time_spent']/60)*g['hourly_rate']
+    f = slice_by_dates(timesheet, date1, date2)
+    cols = ['project', 'time_spent']
+    g = f[cols].groupby('project').agg(np.sum).reset_index()
+    g = g.merge(project_rates_df)
+    g['cost'] = g['time_spent']*g['rate']
     return g
+
+def bill_at_tiered_rates(timesheet, tiers_df,
+  date1=None, date2=None):
+    """
+    Slice the given timesheet to the given dates,
+    group the time spent into periods of (Pandas) frequency
+    ``freq``, and return a data frame with the columns
+
+    ...
+    """
+    pass
 
 # Aggregating
 def agg_by_project(timesheet):
