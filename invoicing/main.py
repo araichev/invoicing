5cbedc5d0d23197f7db24733d8de244423348483
Alex Raichev <alex@raichev.net>
2016-04-18 20:26:09 +1200

    In progress

--- invoicing/main.py
+++ invoicing/main.py
@@ -2,6 +2,10 @@
 Main module.
 
 A *timesheet* is a Pandas data frame with at least the columns...
+
+TODO:
+
+    - Change to ``bill_linear`` and ``bill_piecewise_linear``
 """
 from collections import OrderedDict
 
@@ -9,6 +13,7 @@ import pandas as pd
 import numpy as np 
 from jinja2 import Environment, PackageLoader
 
+TIME_UNITS = ['min', 'h']
 
 # Load Jinja templates
 ENV = Environment(loader=PackageLoader('main', 'templates'))
@@ -19,15 +24,25 @@ def parse_date(date_str):
         return None
     return pd.datetime.strptime(date_str, '%Y%m%d')
 
-def read_timesheet(path):
+def get_convert_to_hours(time_units):
+    if time_units == 'min':
+        return lambda x: x/60
+    elif time_units == 'h':
+        return lambda x: x
+
+def read_timesheet(path, time_units='h'):
     """
     Read a timesheet located at the given path and return the corresponding
     data frame.
     """
+    if time_units not in TIME_UNITS:
+        raise ValueError('time_units must be one of', TIME_UNITS)
+    
     f = pd.read_csv(path, parse_dates=['date'], date_parser=parse_date)
     f = f.sort_values('date')
-    # Convert from minutes to hours
-    f['time_spent'] /= 60
+    # Convert to hours
+    convert_to_hours = get_convert_to_hours(time_units)
+    f['time_spent'] = f['time_spent'].map(convert_to_hours)
     return f
 
 def validate_timesheet(timesheet):
@@ -62,16 +77,56 @@ def bill_at_project_rates(timesheet, project_rates_df,
     g['cost'] = g['time_spent']*g['rate']
     return g
 
-def bill_at_tiered_rates(timesheet, tiers_df,
-  date1=None, date2=None):
-    """
-    Slice the given timesheet to the given dates,
-    group the time spent into periods of (Pandas) frequency
-    ``freq``, and return a data frame with the columns
+def partition_by_tiered_rates(t, tiered_rates_df):
+    f = tiered_rates_df.copy()
+    prev_c = 0
+    time_spent = []
+    for c in f['time_cutoff'].values:
+        delta = c - prev_c
+        if t >= delta:
+            time_spent.append(delta)
+            t -= delta
+        elif t > 0:
+            time_spent.append(t)
+            t = 0
+        else:
+            time_spent.append(0)
+        prev_c = c
+    f['time_spent'] = time_spent
+    return f
 
-    ...
-    """
-    pass
+def bill_at_tiered_rates(timesheet, tiered_rates_df, freq='W', 
+  groupby_project=False, date1=None, date2=None):
+    # Slice
+    timesheet = slice_by_dates(timesheet, date1, date2)
+
+    # Resample to frequency by summing
+    if groupby_project:
+        f = timesheet.groupby('project').apply(
+          lambda x: x.set_index('date')[['time_spent']].resample('W').sum())
+    else:
+        f = timesheet.set_index('date')[['time_spent']].resample('W').sum()
+    
+    # Partition each group by tiered rates
+    def my_agg(group):
+        t = group['time_spent'].iat[0]
+        return partition_by_tiered_rates(t, tiered_rates_df)
+
+    f = f.reset_index().rename(columns={'date': 'period_end'})
+    
+    if groupby_project:
+        cols = ['period_end', 'project']
+        drop_cols = ['level_2']
+    else:
+        cols = ['period_end']
+        drop_cols = ['level_1']
+        
+    g = f.groupby(cols).apply(my_agg).reset_index().drop(drop_cols, axis=1)
+
+    # Compute cost
+    g['cost'] = g['time_spent']*g['rate']
+
+    return g
 
 # Aggregating
 def agg_by_project(timesheet):
